a0d3d7eb3dc011f0df686fb3dfdd56cf17a900e9
Akash Pal <creativeakashpal@gmail.com>
2022-09-27 17:14:26 +0530

    initial commit

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,22 @@
+@extends('layout')
+
+@section('content')
+
+<h2>{{$heading}}</h2>
+
+@unless(count($listings) == 0) 
+
+
+@foreach($listings as $listing)
+<h2>
+    <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
+</h2>
+<p>{{$listing['description']}}</p>
+@endforeach
+
+@else 
+    <p>No Listings Found</p>
+
+@endunless
+
+@endsection
